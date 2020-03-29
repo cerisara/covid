56261198556e2a0c71fb775c0ef8ba003060a6a2
xtof <cerisara@loria.fr>
2020-03-29 19:26:21 +0200

    ok

--- src/predict.py
+++ src/predict.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import string
+import corpus
+from showdata import NB
 
 embdim=50
 winlen=20
@@ -26,4 +29,40 @@ class Mod(nn.Module):
         z = self.lastlay(z)
         z = z.view(-1,8,ndigs)
         return z
+
+def get8digs(s):
+    if len(s)==0||len(s)>8: return []
+    r = []
+    for c in s:
+        r.append(ord(c)-ord('0'))
+    r = [0]*8-len(s) + r
+    return r
+
+def train():
+    nbs = NB()
+    chars = string.printable
+    nchars = len(chars)
+    voc = {}
+    for c in chars: voc[c]=len(voc)
+    m = Mod(nchars)
+    for utts in corpus.loadData():
+        for u in utts:
+            n=nbs.findall(u)
+            if n>0:
+                for a,b in nbs.nbs:
+                    s=[]
+                    i=1
+                    while a-i>=0 and len(s)<winlen:
+                        c=u[a-i]
+                        if c in voc: s.append(voc[c])
+                        i+=1
+                    if len(s)==winlen:
+                        goldy = get8digs(u[a,b])
+                        x = torch.tensor(s)
+                        x = x.view(1,winlen)
+                        y = m(x)
+                        print(y)
+                        exit()
+
+train()
 
